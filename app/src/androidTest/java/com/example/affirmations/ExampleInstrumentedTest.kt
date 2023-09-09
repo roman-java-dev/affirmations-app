e6a4e6c17d87dba8000f5e618abb362ad0ddd530
Roman Kinash <romankinash09@gmail.com>
2023-09-09 14:42:35 +0300

    added project

---
+++ app/src/androidTest/java/com/example/affirmations/ExampleInstrumentedTest.kt
@@ -0,0 +1,44 @@
+package com.example.affirmations
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.affirmations", appContext.packageName)
+    }
+
+    @Test
+    fun scroll_to_item(){
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9)
+        )
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed()))
+    }
+}
